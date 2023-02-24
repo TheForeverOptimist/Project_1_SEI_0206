b865eeae4a623f58f62d36bbb56ad7d90232fc9d
TheForeverOptimist <sean@elevate.app>
2023-02-24 11:48:59 -0500

    made some positive steps

--- project.js
+++ project.js
@@ -1,6 +1,31 @@
 const myButton = document.getElementById("myButton");
 
 myButton.addEventListener("click", function(){
-    alert("Choose who goes first! You or The Computer?");
     myButton.style.display = "none";
 });
+
+const gif = document.getElementById("gif");
+
+let xPosition = 0;
+let yPosition = 0;
+let moveIt = false;
+
+function moveGif() {
+    xPosition += 5;
+    yPosition += 2;
+    gif.style.left = xPosition + "px"
+    gif.style.left = yPosition + "px";
+
+    //check if gif reached center
+    if(xPosition >= 900) {
+        cancelAnimationFrame(moveGif);
+        // moveIt = false;
+    } else {
+        requestAnimationFrame(moveGif);
+    }
+    }
+    // document.getElementById(myButton).addEventListener("click", function(){
+    //     if(!moveIt) {
+            moveGif();
+    //     }
+    // });
