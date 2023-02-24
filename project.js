6e22403ba4db7fb1066fb6e89f29f0c879bc9a6f
TheForeverOptimist <sean@elevate.app>
2023-02-24 11:18:39 -0500

    just made a mistake need to save more often

---
+++ project.js
@@ -0,0 +1,6 @@
+const myButton = document.getElementById("myButton");
+
+myButton.addEventListener("click", function(){
+    alert("Choose who goes first! You or The Computer?");
+    myButton.style.display = "none";
+});
