e066a172e746eaff4ebd111d4496b032546cf34a
TheForeverOptimist <sean@elevate.app>
2023-02-24 12:50:21 -0500

    I'm definitely about to break some stuff

--- project.js
+++ project.js
@@ -1,8 +1,19 @@
 const myButton = document.getElementById("myButton");
+const strikeBall = document.getElementById("strikeBall");
 
 myButton.addEventListener("click", function(){
     myButton.style.display = "none";
 });
+
+// strikeBall.addEventListener("click", function(){
+//     if(gif.paused) {
+//         gif.play();
+//         strikeBall.textContent = "Pause";
+//     } else {
+//         gif.pause();
+//         strikeBall.textContent = "Play";
+//     }
+// });
 
 const gif = document.getElementById("gif");
 
@@ -17,15 +28,16 @@ function moveGif() {
     gif.style.left = yPosition + "px";
 
     //check if gif reached center
-    if(xPosition >= 900) {
+    if(xPosition >= 1460) {
         cancelAnimationFrame(moveGif);
-        // moveIt = false;
+        moveIt = false;
     } else {
         requestAnimationFrame(moveGif);
     }
     }
-    // document.getElementById(myButton).addEventListener("click", function(){
-    //     if(!moveIt) {
+    document.getElementById("myButton").addEventListener("click", function(){
+        if(!moveIt) {
+            moveIt = true;
             moveGif();
-    //     }
-    // });
+        }
+    });
